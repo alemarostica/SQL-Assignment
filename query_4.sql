5b06e872ada1eef684e59a6e3e61ed097aca9aa1
alemarostica <alemarostica@virgilio.it>
2022-11-25 23:11:35 +0100

    Quarta Query

---
+++ query_4.sql
@@ -0,0 +1,6 @@
+select
+	directorawards.award,
+	directorawards.year,
+	directorawards.director
+from movies right join directorawards on movies.director = directorawards.director
+where movies.gross > 1000000 and movies.year > (extract(year from current_date) - 5)
