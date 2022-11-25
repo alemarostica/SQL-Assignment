f7ba96bdd9069c0542e32111ac8422c35536a28c
alemarostica <alemarostica@virgilio.it>
2022-11-25 11:48:11 +0100

    first query

---
+++ query_1.sql
@@ -0,0 +1,11 @@
+select distinct
+	directors.director,
+	coalesce(min(movies.gross), -1), 
+	coalesce(max(movies.gross), -1), 
+	trunc(coalesce(avg(movies.gross), -1), 2)
+from directors left join movies
+on directors.director = movies.director
+where directors.yearofbirth < (extract(year from current_date) - 50)
+group by directors.director
+order by 4 asc
+
