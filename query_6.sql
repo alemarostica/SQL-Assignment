0c45695ce917728d00f350bce57cb0856b24dc8e
alemarostica <alemarostica@virgilio.it>
2022-11-27 14:23:15 +0100

    Sesta Query con v2

--- query_6.sql
+++ query_6.sql
@@ -8,3 +8,13 @@ where m.count = (select max(m.count) from (select movieawards.title as name, cou
 											from movieawards
 											where award like 'Oscar%' and result = 'won'
 											group by name) as m)
+
+--v2
+with T as (select movieawards.title as name, count(movieawards.award)
+		from movieawards
+		where award like 'Oscar%' and result = 'won'
+		group by name
+)
+select T.name, T.count
+from T
+where T.count = (select max(T.count) from T)
