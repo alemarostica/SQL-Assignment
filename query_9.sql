06d952872aef908531194fe4e40aea224417d8b5
alemarostica <alemarostica@virgilio.it>
2022-11-28 17:58:04 +0100

    Nona Query

---
+++ query_9.sql
@@ -0,0 +1,11 @@
+with awardsandgross as(
+    select movies.director,
+        movies.gross - movies.budget as profit,
+        movieawards.award
+    from movies
+        right join movieawards on movies.title = movieawards.title
+    where movieawards.award like '%Best Director'
+)
+select *
+from awardsandgross
+where profit < 0
