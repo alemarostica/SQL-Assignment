5e2e58017c80cc53aec2b4b667978e2d62624ac7
alemarostica <alemarostica@virgilio.it>
2022-11-28 18:01:18 +0100

    Removed redundancy

--- query_9.sql
+++ query_9.sql
@@ -1,5 +1,6 @@
 with awardsandgross as(
     select movies.director,
+        movieawards.title,
         movies.gross - movies.budget as profit,
         movieawards.award
     from movies
@@ -8,4 +9,8 @@ with awardsandgross as(
 )
 select *
 from awardsandgross
-where profit < 0;
+where profit < 0
+group by awardsandgross.director,
+    awardsandgross.title,
+    awardsandgross.profit,
+    awardsandgross.award
