6ae8ba5abf8442286affff5fc2c17bb52bac3098
alemarostica <alemarostica@virgilio.it>
2022-11-28 16:23:19 +0100

    Quinta Query

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SQL-Assignment
 SQL Queries for Database Assignment 2022
+
+Niente è finito, tutto è provvisorio
+
+(a meno che non sia specificato finito)
---
+++ query_5.sql
@@ -0,0 +1,29 @@
+with oscarswon as (
+    select movieawards.title,
+        movies.year,
+        count(movieawards.award)
+    from movieawards
+        left join movies on movies.title = movieawards.title
+    where movieawards.result = 'won'
+    group by movieawards.title,
+        movies.year
+),
+oscarsnominated as (
+    select movieawards.title,
+        movies.year,
+        count(movieawards.award)
+    from movieawards
+        left join movies on movies.title = movieawards.title
+    group by movieawards.title,
+        movies.year
+)
+select oscarsnominated.title,
+    coalesce(
+        trunc(
+            ((oscarswon.count * 1.0) / oscarsnominated.count) * 100,
+            2
+        ),
+        0
+    )
+from oscarsnominated
+    left join oscarswon on oscarsnominated.title = oscarswon.title
